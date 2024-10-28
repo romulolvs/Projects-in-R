d4a9c289684f3342a7328685d2c576c23037bc21
Rômulo Silva <romulo.leonardo@gmail.com>
2024-10-28 19:39:03 -0300

    New files

---
+++ 02-linguagem-r/99-Exercicios.R
@@ -0,0 +1,13 @@
+####################################################################################################
+# Exercícios do Capítulo 02
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("C:/GitHubProjects/r_language/Projects-in-R/02-linguagem-r")
+getwd()
+
+####################################################################################################
